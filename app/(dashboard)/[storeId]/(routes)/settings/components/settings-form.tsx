3b0ce8a4aebeee7618054f526bd9404ca2ffad6e
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-07-16 18:27:59 -0600

    Created <AlertModal/> component and modified <SettingsForm/> component to add that component. Also, added <ApiAlert/> component implmenting shadcn alert & badge component

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -22,6 +22,9 @@ import {
 } from '@/components/ui/form';
 import { Separator } from '@/components/ui/separator';
 import Heading from '@/components/ui/heading';
+import AlertModal from '@/components/modals/alert-modal';
+import ApiAlert from '@/components/ui/api-alert';
+import { useOrigin } from '@/hooks/use-origin';
 
 const formSchema = z.object({
   name: z.string().min(2),
@@ -36,6 +39,7 @@ type Props = {
 export default function SettingsForm({ initialData }: Props) {
   const params = useParams();
   const router = useRouter();
+  const origin = useOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -85,6 +89,12 @@ export default function SettingsForm({ initialData }: Props) {
 
   return (
     <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
       <div className="flex items-center justify-between">
         <Heading
           title="Store settings"
@@ -129,6 +139,12 @@ export default function SettingsForm({ initialData }: Props) {
           </Button>
         </form>
       </Form>
+      <Separator />
+      <ApiAlert
+        title="NEXT_PUBLIC_API_URL"
+        variant="public"
+        description={`${origin}/api/${params.storeId}`}
+      />
     </>
   );
 }
