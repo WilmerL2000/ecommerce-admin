489e3d0e93151e5633b9490a48f36512fe7e67ab
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-07-23 13:41:04 -0600

    Created Sizes & Size page. Also, <SizeForm/>, <CellAction> & <SizeClient/> components and columns const for data table headers & cell actions. Finally, were created sizes api routes for CRUD operations. As well, Size & Color prisma schema

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/components/size-form.tsx
@@ -0,0 +1,176 @@
+'use client';
+
+import * as z from 'zod';
+import axios from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
+import { Trash } from 'lucide-react';
+import { Size } from '@prisma/client';
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Separator } from '@/components/ui/separator';
+import Heading from '@/components/ui/heading';
+import AlertModal from '@/components/modals/alert-modal';
+import ImageUpload from '@/components/ui/image-upload';
+
+const formSchema = z.object({
+  name: z.string().min(2),
+  value: z.string().min(1),
+});
+
+type SizeFormValues = z.infer<typeof formSchema>;
+
+type Props = {
+  initialData: Size | null;
+};
+
+export default function SizeForm({ initialData }: Props) {
+  const params = useParams();
+  const router = useRouter();
+
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const title = initialData ? 'Edit size' : 'Create size';
+  const description = initialData ? 'Edit a size.' : 'Add a new size';
+  const toastMessage = initialData ? 'Size updated.' : 'Size created.';
+  const action = initialData ? 'Save changes' : 'Create';
+
+  const form = useForm<SizeFormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+      name: '',
+      value: '',
+    },
+  });
+
+  /**
+   * The `onSubmit` function is used to handle form submission in a TypeScript React application,
+   * making an API request to update or create a size and displaying success or error messages.
+   * @param {SizeFormValues} data - The `data` parameter is an object of type
+   * `SizeFormValues`. It represents the data that will be sent to the server when submitting the
+   * form.
+   */
+  const onSubmit = async (data: SizeFormValues) => {
+    try {
+      setLoading(true);
+      if (initialData) {
+        await axios.patch(
+          `/api/${params.storeId}/sizes/${params.sizeId}`,
+          data
+        );
+      } else {
+        await axios.post(`/api/${params.storeId}/sizes`, data);
+      }
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success(toastMessage);
+    } catch (error: any) {
+      toast.error('Something went wrong.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  /**
+   * The `onDelete` function is used to delete a size, display a success message, and handle any
+   * errors that may occur.
+   */
+  const onDelete = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/sizes/${params.sizeId}`);
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success('Size deleted.');
+    } catch (error: any) {
+      toast.error('Make sure you removed all products using this size first.');
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
+      />
+      <div className="flex items-center justify-between">
+        <Heading title={title} description={description} />
+        {initialData && (
+          <Button
+            disabled={loading}
+            variant="destructive"
+            size="sm"
+            onClick={() => setOpen(true)}
+          >
+            <Trash className="h-4 w-4" />
+          </Button>
+        )}
+      </div>
+      <Separator />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
+          <div className="md:grid md:grid-cols-3 gap-8">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Size name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="value"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Value</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Size value"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button disabled={loading} className="ml-auto" type="submit">
+            {action}
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+}
---
+++ app/(dashboard)/[storeId]/(routes)/sizes/[sizeId]/page.tsx
@@ -0,0 +1,22 @@
+import prismadb from '@/lib/prismadb';
+import SizeForm from './components/size-form';
+
+export default async function SizePage({
+  params,
+}: {
+  params: { sizeId: string };
+}) {
+  const size = await prismadb.size.findUnique({
+    where: {
+      id: params.sizeId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeForm initialData={size} />
+      </div>
+    </div>
+  );
+}
