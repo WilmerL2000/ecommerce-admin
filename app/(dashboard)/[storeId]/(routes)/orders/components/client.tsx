23522b1207bf7501dccfe3403617beb2b5fa25f3
WilmerL2000 <87109442+WilmerL2000@users.noreply.github.com>
2023-07-23 20:08:33 -0600

    Adding Orders page. Also, <OrderClient/> component and Order columns for data table headers. As well, products api routes with CRUD operations. Finally, adding Order and OrderItem models into prisma schema

---
+++ app/(dashboard)/[storeId]/(routes)/orders/components/client.tsx
@@ -0,0 +1,21 @@
+import Heading from '@/components/ui/heading';
+import { Separator } from '@/components/ui/separator';
+import { DataTable } from '@/components/ui/data-table';
+import { OrderColumn, columns } from './columns';
+
+type Props = {
+  data: OrderColumn[];
+};
+
+export default function OrderClient({ data }: Props) {
+  return (
+    <>
+      <Heading
+        title={`Orders (${data.length})`}
+        description="Manage orders for your store"
+      />
+      <Separator />
+      <DataTable columns={columns} data={data} searchKey="label" />
+    </>
+  );
+}
--- app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/components/client.tsx
@@ -32,7 +32,7 @@ export default function ProductClient({ data }: Props) {
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
+      <DataTable columns={columns} data={data} searchKey="name" />
       <Heading title="API" description="API Calls for Products" />
       <Separator />
       <ApiList entityName="products" entityIdName="productId" />
